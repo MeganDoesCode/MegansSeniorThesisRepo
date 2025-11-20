73d1debc40de24f3e12c377e53d9dbec1c21b9a5
Megan Jensen <spottedeye2@gmail.com>
2025-11-20 10:56:14 -0600

    I finally got the database working

--- code/index.js
+++ code/index.js
@@ -5,19 +5,24 @@ const morgan = require('morgan');
 const bodyParser = require("body-parser"); 
 const DBAbstraction = require('./DBAbstraction'); 
  
-const db = new DBAbstraction('../Product Database.db'); 
+const db = new DBAbstraction('ProductDatabase.db'); 
  
 const app = express(); 
  
 app.use(morgan('dev')); 
 app.use(bodyParser.urlencoded({ extended: false })); 
 app.use(bodyParser.json()); 
- 
-app.use((req, res) => { 
-    res.status(404).send(`<h2>Uh Oh!</h2><p>Sorry ${req.url} cannot be found here</p>`); 
-}); 
 
-app.get('/products', async (req, res) => {
+db.init() 
+    .then(() => { 
+        app.listen(8080, () => console.log('The server is up and running...')); 
+    }) 
+    .catch(err => { 
+        console.log('Problem setting up the database'); 
+        console.log(err); 
+    });
+
+app.get('/', async (req, res) => {
     try{
         const products = await db.getProducts();
         if(products) {
@@ -29,13 +34,8 @@ app.get('/products', async (req, res) => {
         res.json({"results" : "none"});
     }
 });
- 
-db.init() 
-    .then(() => { 
-        app.listen(53140, () => console.log('The server is up and running...')); 
-    }) 
-    .catch(err => { 
-        console.log('Problem setting up the database'); 
-        console.log(err); 
-    });
+
+app.use((req, res) => { 
+    res.status(404).send(`<h2>Uh Oh!</h2><p>Sorry ${req.url} cannot be found here</p>`); 
+}); 
 
